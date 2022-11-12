3fd95c78f4846c325063a43e64b7a06b9a52849e
James Rozsypal <jimmerxl@gmail.com>
2022-11-12 13:43:20 -0500

    Make App a class component and add constructor for tracking tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div className='App'>
-      <h1>Task App</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: '' },
+      tasks: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Task App</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
