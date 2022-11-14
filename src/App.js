2b9bb7a484495a1490c469b0b1cbeef5a84c3235
James Rozsypal <jimmerxl@gmail.com>
2022-11-14 13:17:14 -0500

    Add input elements and define state variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './styles/App.css';
+import Overview from './components';
 
 class App extends Component {
   constructor() {
@@ -11,9 +13,16 @@ class App extends Component {
   }
 
   render() {
+    const { task, tasks } = this.state;
+
     return (
       <div className='App'>
         <h1>Task App</h1>
+        <div className='input'>
+          <label htmlFor='task-input'>Enter a task:</label>
+          <input type='text' name='task-input' id='task-input' />
+          <button>Add Task</button>
+        </div>
       </div>
     );
   }
