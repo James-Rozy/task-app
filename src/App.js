a700432e59e366278039cf4aec68ffab90394cdb
James Rozsypal <jimmerxl@gmail.com>
2022-11-15 12:38:55 -0500

    Add functions for handeling changes to state when user adds a task and add Overview component for displaying the task list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import Overview from './components';
+import uniqid from 'uniqid';
+import Overview from './components/Overview';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      task: { text: '' },
+      task: { text: '', id: uniqid() },
       tasks: [],
     };
   }
+
+  onChangeTask = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '', id: uniqid() },
+    });
+  };
 
   render() {
     const { task, tasks } = this.state;
@@ -18,11 +36,20 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Task App</h1>
-        <div className='input'>
-          <label htmlFor='task-input'>Enter a task:</label>
-          <input type='text' name='task-input' id='task-input' />
-          <button>Add Task</button>
-        </div>
+        <form action='#' className='task-form' onSubmit={this.onSubmitTask}>
+          <div className='input'>
+            <label htmlFor='task-input'>Enter a task:</label>
+            <input
+              type='text'
+              name='task-input'
+              id='task-input'
+              onChange={this.onChangeTask}
+              value={task.text}
+            />
+            <button type='submit'>Add Task</button>
+          </div>
+        </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
