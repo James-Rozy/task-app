aa9847393acffb6beca85db36fe2098967a75b14
James Rozsypal <jimmerxl@gmail.com>
2022-11-16 10:26:48 -0500

    Add edit and delete buttons

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -10,6 +10,10 @@ const Overview = (props) => {
           <li key={task.id}>
             <div className='li-number'>{task.number}</div>
             <div className='li-text'>{task.text}</div>
+            <div className='li-controls'>
+              <button className='edit btn'>Edit</button>
+              <button className='delete btn'>Delete</button>
+            </div>
           </li>
         );
       })}
